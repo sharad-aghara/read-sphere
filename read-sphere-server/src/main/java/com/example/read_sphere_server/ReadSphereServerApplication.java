acfaea1d8cab0b4e369129a43d38d00b61232bdd
sharad-aghara <sharadaghara24@gmail.com>
2024-07-15 15:55:02 +0530

    Initial Commit

---
+++ read-sphere-server/src/main/java/com/example/read_sphere_server/ReadSphereServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.read_sphere_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadSphereServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadSphereServerApplication.class, args);
+	}
+
+}
---
+++ read-sphere-server/src/test/java/com/example/read_sphere_server/ReadSphereServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.read_sphere_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadSphereServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
