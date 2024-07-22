cfcef42b3beefb0b13048efa00d8c4adadf8b795
Sharad Aghara <sharadaghara24@gmail.com>
2024-07-22 12:52:41 +0530

    Implemented AuditorAware functionality

---
+++ read-sphere-server/src/main/java/com/example/read_sphere_server/config/ApplicationAuditAware.java
@@ -0,0 +1,25 @@
+package com.example.read_sphere_server.config;
+
+import com.example.read_sphere_server.model.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+// AuditorAware is type of Generic - and Id of User is type Integer
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null ||
+            !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());  // if encase userPrincipal.getId() is null this will return null
+    }
+}
