dd51dfb406b45453464d83933e9d4c2f25f23ff4
Sharad Aghara <sharadaghara24@gmail.com>
2024-07-25 18:52:52 +0530

    Implemented ActivationTokenException class

---
+++ read-sphere-server/src/main/java/com/example/read_sphere_server/exception/ActivationTokenException.java
@@ -0,0 +1,7 @@
+package com.example.read_sphere_server.exception;
+
+public class ActivationTokenException extends RuntimeException{
+    public ActivationTokenException(String message) {
+        super(message);
+    }
+}
