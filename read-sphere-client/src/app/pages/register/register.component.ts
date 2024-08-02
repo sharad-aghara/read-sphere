349bcc8e235722c2f966854fe1d7345c83b267f9
Sharad Aghara <sharadaghara24@gmail.com>
2024-08-02 16:59:32 +0530

    created RegisterComponent

--- read-sphere-client/src/app/pages/register/register.component.ts
+++ read-sphere-client/src/app/pages/register/register.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { RegistrationRequest } from '../../services/models';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/services';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  registerRequest: RegistrationRequest = { email: '', firstname: '', lastname: '', password: '' };
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+  
+  login() {
+    this.router.navigate(['login']);
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    })
+    .subscribe({
+      next: () => {
+        this.router.navigate(['activate-account']);
+      },
+      error: (err) => {
+        this.errorMsg = err.error.validationErrors;
+      }
+    })
+  }
 }
