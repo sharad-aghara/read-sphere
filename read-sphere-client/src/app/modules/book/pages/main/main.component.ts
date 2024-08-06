7bc3a4c815131e401a3c8431ebab0459af2871af
Sharad Aghara <sharadaghara24@gmail.com>
2024-08-06 10:00:08 +0530

    modified main-component

--- read-sphere-client/src/app/modules/book/pages/main/main.component.ts
+++ read-sphere-client/src/app/modules/book/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../../components/menu/menu.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
--- read-sphere-client/src/main.ts
+++ read-sphere-client/src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpTokenInterceptor } from './app/services/interceptor/http-token.interceptor';
+import { TokenService } from './app/services/token/token.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      provideHttpClient(
+        withInterceptors([HttpTokenInterceptor])
+      )
+    ),
+    TokenService
+  ]
+}).catch(err => console.error(err));
