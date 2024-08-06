72110c2ac9245b5dbc7254a60e8dc9c14f9029f1
Sharad Aghara <sharadaghara24@gmail.com>
2024-08-06 18:37:57 +0530

    added provider http-token-interceptor

--- read-sphere-client/src/app/app.config.ts
+++ read-sphere-client/src/app/app.config.ts
@@ -3,13 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+    }
   ]
 };
--- read-sphere-client/src/main.ts
+++ read-sphere-client/src/main.ts
@@ -1,17 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { HttpTokenInterceptor } from './app/services/interceptor/http-token.interceptor';
-import { TokenService } from './app/services/token/token.service';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      provideHttpClient(
-        withInterceptors([HttpTokenInterceptor])
-      )
-    ),
-    TokenService
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
